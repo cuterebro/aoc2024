ce55e3a2da515fafc5986c221563fac8c3df03d9
Cuterebro <cuterebro@gmail.com>
2024-12-09 19:48:09 +0300

    day 6 part 2

--- lib/gallivant.ml
+++ lib/gallivant.ml
@@ -2,6 +2,15 @@ open Core
 
 type journey_result = Finished of int | Looped
 
+let print_field field = 
+  Array.iter ~f:(fun row -> print_endline (Bytes.to_string row)) field
+
+let field_copy field = Array.map ~f:Bytes.copy field
+
+let print_result res : unit = match res with
+  | Finished count -> printf "Finished in %d\n" count
+  | Looped -> print_endline "Looped"
+
 let start_position lines : (int * int) = 
   let aux line = match Stdlib.Bytes.index_opt line '^' with Some x -> x | None -> -1 in
   let res : int array = Array.map ~f:aux lines in
@@ -21,6 +30,13 @@ let rotate dir = match dir with
   | (0, -1) -> (-1, 0)
   | _ -> failwith "Wrong direction"
 
+let char_of_dir dir = match dir with   
+  | (-1, 0) -> 'w'
+  | (0, 1) -> 'd'
+  | (1, 0) -> 's'
+  | (0, -1) -> 'a'
+  | _ -> 'X'
+
 let get_char (field : bytes array) pos : char = let (row, col) = pos in Bytes.get field.(row) col  
 let set_char (field : bytes array) pos (c : char) : unit = let (row, col) = pos in Bytes.set field.(row) col c
 let next pos dir = 
@@ -31,23 +47,36 @@ let journey field position : journey_result =
   let rows = Array.length field in
   let cols = Bytes.length (Array.get field 0) in
   let finished pos = let (row, col) = pos in row < 0 || col < 0 || row >= rows || col >= cols in
-  let visit pos = 
-    let count = if phys_equal (get_char field pos) 'X' then 0 else 1 in set_char field pos 'X'; count
-  in
   let is_obstacle pos = phys_equal (get_char field pos) '#' in
-  let rec step pos dir count : int =
+  let dejavu pos trace = phys_equal (get_char field pos) trace in
+  let visit pos trace = 
+    let count = if String.contains "wdsaX" (get_char field pos) then 0 else 1 in set_char field pos trace; count
+  in
+  let rec step pos dir count : journey_result =
+    let trace = char_of_dir dir in
     let next_pos = next pos dir in
     match finished next_pos with
-      | true -> count
-      | false -> match is_obstacle next_pos with
-        | true -> step pos (rotate dir) count
-        | false -> step next_pos dir count + (visit pos)
-   in Finished (step position (-1, 0) 1)
+      | true -> Finished count
+      | false -> match dejavu pos trace with
+        | true -> Looped
+        | false -> match is_obstacle next_pos with
+          | true -> step pos (rotate dir) count
+          | false -> step next_pos dir (count + (visit pos trace))
+   in step position (-1, 0) 1
 
-let print_field field = 
-  Array.iter ~f:(fun row -> print_endline (Bytes.to_string row)) field
+let count_loops field start : int =
+  let points = Array.mapi ~f:(fun i row ->
+    (List.init (Bytes.length row) ~f:(fun x -> x)) |> List.filter ~f:(fun j -> phys_equal (Bytes.get row j) '.') |> List.map ~f:(fun j -> (i, j))
+  ) field |> Array.to_list |> List.concat in
+  List.fold ~init:0 ~f:(fun acc pos -> 
+    let test_field = field_copy field in
+    set_char test_field pos '#';
+    match journey test_field start with Finished _ -> acc | Looped -> acc + 1
+  ) points
 
 let exec filename =
   let (field, position) = read_file filename in
-  let first_result = journey (Array.copy field) position in
-  printf "%d\n" first_result
+  let first_result = journey (field_copy field) position in
+  let second_result = count_loops field position in
+    print_result first_result;
+    printf "%d\n" second_result;
